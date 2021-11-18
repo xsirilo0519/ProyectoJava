e36718a828208fc72fdc7e40ded9d9833330eb18
sebas <sebaslpez0519@gmail.com>
2021-11-18 15:32:19 -0500

    Actividad 8 terminada

--- src/main/java/Ejercicio_8/mainEjercicio8.java
+++ src/main/java/Ejercicio_8/mainEjercicio8.java
@@ -7,6 +7,23 @@ import java.util.Locale;
 
 public class mainEjercicio8 {
     public mainEjercicio8(Utils util){
-
+        util.showMessage("Dia laboral");
+        String dia=util.inputStringMessage("Ingrese un dia de la semana").toLowerCase();
+        switch (dia.toLowerCase()){
+            case "lunes":
+            case "martes":
+            case "miercoles":
+            case "jueves":
+            case "vienes":
+                util.showMessage("El dia "+dia+" es un dia laboral");
+                break;
+            case "sabado":
+            case "domingo":
+                util.showMessage("El dia "+dia+" no es un dia laboral");
+                break;
+            default:
+                util.showMessage("No ingreso un dia valido");
+                break;
+        }
     }
 }
